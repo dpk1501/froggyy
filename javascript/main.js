cf5380534a8b04f0f859c358eb89094cf4435696
dpk1501 <112957142+dpk1501@users.noreply.github.com>
2023-03-17 14:59:53 +0300

    all

--- javascript/main.js
+++ javascript/main.js
@@ -100,6 +100,7 @@ let obstacleWidth = 30;
 let obstacleHeight = Math.floor(Math.random() * 50) + 50;
 
 let greenClicked = 0
+let leafClicked = 0
 
 function moveObstacle() {
     let obstacle = document.querySelector('.obstacle');
@@ -119,11 +120,18 @@ function moveObstacle() {
           $('.block3').show();
           $('.block2').hide();
         }
+        else if (leafClicked == 0){
+          $('.block4').show();
+          $('.block2').hide();
+        }
         else {
-          return false;
+          $('.block5').show();
+          $('.block2').hide();
+        }
+
         }
     }
-}
+
 
 
 
@@ -135,7 +143,7 @@ window.setInterval(function(){
       'background-color' : randomColor,
     });
 
-  }, 3000);
+  }, 5000);
 
 
 // ПРЫЖКИ ПО КЛИКУ И НАЖАТИЮ
@@ -151,11 +159,24 @@ document.addEventListener('keydown', control);
 
 // ПЕРВАЯ МИНИ ИГРА
 
-setTimeout(function () {
   setInterval(function(){
     $('#red').css('background', 'grey');
-  }, 1000);
-  })
+  }, 500); 
+  setInterval(function(){
+    $('#green').css('background', 'lime');
+  }, 1000); 
+  setInterval(function(){
+    $('#yellow').css('background', 'yellow');
+  }, 1000); 
+  setInterval(function(){
+    $('#red').css('background', 'red');
+  }, 1300);
+  setInterval(function(){
+    $('#green').css('background', 'grey');
+  }, 1300);
+  setInterval(function(){
+    $('#yellow').css('background', 'grey');
+  }, 1500);
 
 
 $(function() {
@@ -189,6 +210,7 @@ $( function() {
 
 
 // ВТОРАЯ МИНИ ИГРА
+
 $('.green').click(function(){
   greenClicked += 1;
   $('.block3').hide();
@@ -201,17 +223,140 @@ $('.green').click(function(){
   let obstacleInterval = setInterval(moveObstacle, 5);
  });
 
+ $('.ewleaf1').click(function(){
+  $(this).hide();
+ });
+ $('.ewleaf2').click(function(){
+  $(this).hide();
+ });
+ $('.ewleaf3').click(function(){
+  $(this).hide();
+ });
+ $('.ewleafimposter').click(function(){
+  $(this).hide();
+ });
+
+ $('.uberi1').click(function(){
+  $('.helpfrog').show();
+ })
+
+ $('.helpfrog').click(function(){
+  $('.block4').hide();
+  $('.block2').show();
+ })
+
 
 
 
 // ТРЕТЬЯ МИНИ ИГРА
-$('player').keydown(function(event) {
-  switch (event.which) {
-      case 37: // left arrow key
-          $('#bottom_arrow').animate({ 'left': '-=100' });
+
+$('.helpfrog').click(function(){
+  leafClicked += 1;
+  $('.block4').hide();
+  $('.block2').show();
+});
+
+let player = document.querySelector('.player');
+window.addEventListener('load', () => {
+    player.style.position = 'absolute';
+    player.style.left = 0;
+    player.style.bottom = 0;
+});
+
+// let playerLeft = parseInt(window.getComputedStyle(player).getPropertyValue ('left'));
+// let isMoving;
+// let uppTime;
+// let downnTime;
+
+// function movecharacter() {
+//   if(isMoving) return;
+//   uppTime = setInterval(() => {
+//       if(playerLeft >= 0){
+//           clearInterval(uppTime);
+//           downnTime = setInterval(() => {
+//               if(playerLeft <= 0){
+//                   clearInterval(downnTime);
+//                   isMoving = false;
+//               }
+//               playerLeft -= 0;
+//               player.style.left = playerLeft +'px';
+//               }, 20);
+//       }
+//     playerLeft += 10;
+//     player.style.left = playerLeft + 'px';
+//     isMoving = true;
+//     }, 20); }
+  
+  
+
+let playerLeft = 0;
+let playerBottom = 0;
+
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+      case 'ArrowLeft':
+          playerLeft -= 50;
+          player.style.left = playerLeft + 'px';
           break;
-          case 39: // right arrow key
-          $('#bottom_arrow').animate({ 'left': '+=100' });
+      case 'ArrowRight':
+          playerLeft += 50;
+          player.style.left = playerLeft + 'px';
           break;
   }
 });
+
+
+
+let triangleTop = -100;
+let triangleLeft1 = Math.random() * 2000;
+let triangleLeft2 = Math.random() * 2000;
+let triangleLeft3 = Math.random() * 2000;
+let triangleLeft4 = Math.random() * 2000;
+let triangleLeft5 = Math.random() * 2000;
+
+function moveTriangle() {
+  let triangle1 = document.getElementById('enemy1');
+  let triangle2 = document.getElementById('enemy2');
+  let triangle3 = document.getElementById('enemy3');
+  let triangle4 = document.getElementById('enemy4');
+  let triangle5 = document.getElementById('enemy5');
+
+  triangle1.style.top = triangleTop + 'px';
+  triangle1.style.left = triangleLeft1 + 'px';
+  triangle2.style.top = triangleTop + 'px';
+  triangle2.style.left = triangleLeft2 + 'px';
+  triangle3.style.top = triangleTop + 'px';
+  triangle3.style.left = triangleLeft3 + 'px';
+  triangle4.style.top = triangleTop + 'px';
+  triangle4.style.left = triangleLeft4 + 'px';
+  triangle5.style.top = triangleTop + 'px';
+  triangle5.style.left = triangleLeft5 + 'px';
+  triangleTop += 50;
+
+  function detectHit(triangleLeft){
+    if (playerLeft >= triangleLeft - 50 && playerLeft <= triangleLeft + 50 && triangleTop >= 900) {
+      // $('.block6').show();
+      // $('.block5').hide();
+    }
+  }
+
+
+  detectHit(triangleLeft1);
+  detectHit(triangleLeft2);
+  detectHit(triangleLeft3);
+  detectHit(triangleLeft4);
+  detectHit(triangleLeft5);
+
+  if (triangleTop == 1000){
+    triangleTop = -100;
+    triangleLeft1 = Math.random() * 2000;
+    triangleLeft2 = Math.random() * 2000;
+    triangleLeft3 = Math.random() * 2000;
+    triangleLeft4 = Math.random() * 2000;
+    triangleLeft5 = Math.random() * 2000;
+  }
+
+}
+
+let triangleInterval = setInterval(moveTriangle, 50);
+
